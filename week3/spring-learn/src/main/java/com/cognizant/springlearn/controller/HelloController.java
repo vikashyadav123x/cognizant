0e0a7ce13582a87f284e203092a7e74c6074fd1b
vikash-yadav <vikash@example.com>
2026-06-29 19:56:37 +0530

    Add country REST endpoint in spring-learn

---
+++ week3/spring-learn/src/main/java/com/cognizant/springlearn/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.cognizant.springlearn.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        logger.info("Start sayHello()");
+        String response = "Hello World!!";
+        logger.info("End sayHello()");
+        return response;
+    }
+}
